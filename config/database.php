08a752cb5e9dae64af2243f27b5a8345a4554e97
Ekojoka Chris Idakwo <chris.idakwo@gmail.com>
2019-04-11 16:11:02 +0100

    Fixes Issue #5.
    Added config/app.php file.

--- config/database.php
+++ config/database.php
@@ -1,11 +1,5 @@
 <?php
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
-
 return [
 
     /*
@@ -47,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', $host),
+            'host' => env('DB_HOST', "localhost"),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', $database),
-            'username' => env('DB_USERNAME', $username),
-            'password' => env('DB_PASSWORD', $password),
+            'database' => env('DB_DATABASE', "database"),
+            'username' => env('DB_USERNAME', "homestead"),
+            'password' => env('DB_PASSWORD', "password"),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
