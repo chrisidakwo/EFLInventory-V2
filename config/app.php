127f8e1c7698f22165ccd5ba64f8a0c684d967a1
Ekojoka Chris Idakwo <chris.idakwo@gmail.com>
2019-04-11 16:09:08 +0100

    Updated gitignore and config variables

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Your App Name'),
+    'name' => env('APP_NAME', 'EFLInventory'),
 
     /*
     |--------------------------------------------------------------------------
